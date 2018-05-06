c4c24f57ffc2b59bab30d7dc6ae90af0f913b95f
Koti Vellanki <vellankikoti@gmail.com>
2018-05-06 15:18:58 +0530

    Add files via upload

---
+++ img2pdf.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import os
+file = 'koti.jpg'
+img = Image.open(file)
+
+# if img.mode == 'RGBA':
+# 	img = img.convert('RGB')
+new_file = 'koti.pdf'
+img.save(new_file,'PDF',resolution=100.0)
