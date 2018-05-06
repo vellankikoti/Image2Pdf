1f623c6800d5306a0a00fbfe4988822daf7b39a3
Koti Vellanki <vellankikoti@gmail.com>
2018-05-06 15:20:56 +0530

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -7,3 +7,14 @@ img = Image.open(file)
 # 	img = img.convert('RGB')
 new_file = 'koti.pdf'
 img.save(new_file,'PDF',resolution=100.0)
+
+#If image is not RGB Use the following code
+
+from PIL import Image
+import os
+file = 'koti.jpg'
+img = Image.open(file)
+if img.mode == 'RGBA':
+	img = img.convert('RGB')
+new_file = 'koti.pdf'
+img.save(new_file,'PDF',resolution=100.0)
